e45a7883baf069141722caffefd012006d9ebd88
hamzafattuh2 <hmooza21@gmail.com>
2025-07-17 17:55:10 +0300

    add migration adham

---
+++ database/migrations/2025_07_14_234450_create__bookings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+     public function up(): void {
+        Schema::create('Bookings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('Users')->onDelete('cascade');
+            $table->foreignId('room_id')->constrained('Rooms')->onDelete('cascade');
+            $table->enum('status', ['pending', 'confirmed', 'cancelled', 'finished'])->default('pending');
+            $table->date('check_in_date');
+            $table->date('check_out_date');
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('Bookings');
+    }
+};
